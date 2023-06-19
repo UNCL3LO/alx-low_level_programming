923874df898d40515219f3da6d2593db2592f89a
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-06-19 13:03:37 -0700

    Q5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a statement with proper grammar,
+ *              but the outcome is a piece of art.
+ *
+ * Return: Always 0 (indicating success)
+ */
+int main(void){
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
