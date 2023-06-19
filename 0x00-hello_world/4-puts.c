9a1586168b2788a41e8c6d6800afd75303f972c1
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-06-19 12:13:49 -0700

    Q4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the statement
+ * "Programming is like building a multilingual puzzle"
+ * and returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
