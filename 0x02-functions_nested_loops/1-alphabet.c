fb57b53146740e8ed43c0b06c775661d730df7fc
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-06-20 09:45:07 -0700

    Q2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
 void print_alphabet(void)
 {
-char letter = 'a';
-while (letter <= 'z')
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar(letter);
-letter++;
 }
 _putchar('\n');
 }
+
+/**
+ * _putchar - Writes a character to the standard output
+ * @c: The character to be written
+ *
+ * Return: On success, 1. On error, -1.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
