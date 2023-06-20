1fb07212c6f291018e094d1f48d5cf9f21a2266f
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-06-20 11:42:17 -0700

    Q4 if else loop to check if c is an alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,10 @@
 #include "main.h"
-/**
- * Checking if c is 
+/** Checking if c is 
  * a letter
  */
 int _isalpha(int c)
 {
-if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return (1);
 else
 return (0);
