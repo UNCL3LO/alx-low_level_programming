e4c0239b6170958740840f926e58f549e4207d78
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-06-20 11:49:08 -0700

    Q4 if else loop to check if c is an alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,11 @@
 #include "main.h"
+
 /**
- * Checking if c is 
- * a letter
+ * _isalpha - Checks if a character is an alphabetic character
+ * @c: The character to be checked
+ *
+ * Return: 1 if the character is an alphabetic character,
+ *         0 otherwise
  */
 int _isalpha(int c)
 {
