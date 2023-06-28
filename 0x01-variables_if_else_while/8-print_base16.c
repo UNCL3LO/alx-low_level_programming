b6b4316bdf45ccf673bb37c6e63b45f84bf55ccc
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-06-28 05:29:14 -0700

    Your commit message

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Description: Prints all numbers of base 16 in lowercase,
+ * followed by a new line,
+ * using the putchar function.
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number;
+for (number = 0; number < 16; number++)
+{
+if (number < 10)
+{
+putchar(number + '0');
+}
+else
+{
+putchar(number - 10 + 'a');
+}
+}
+putchar('\n');
+return 0;
+}
