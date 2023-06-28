9216265c36886cd153003d9697366452027e5636
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-06-28 02:42:58 -0700

    Your commit message

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there*/
-/* betty style doc for function main goes there
+/**more headers goes there
+*main -Entry point
 *@n variable to be set to random value
 *Progran to set variable n to random value
 *Returns message
