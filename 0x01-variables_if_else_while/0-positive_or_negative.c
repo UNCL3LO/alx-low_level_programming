56761e3904fca4cf545c36a61367133a1cd163b1
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-06-28 02:30:07 -0700

    Your commit message

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,20 +2,24 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-/* betty style doc for function main goes there */
+/* betty style doc for function main goes there 
+ *@n variable to be set to random value
+*Progran to set variable n to random value
+*Returns message 
+* */
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if (n>0)
+if (n > 0)
 {
 printf("%d is positive\n",n);
 }
-else if(n==0)
+else if (n == 0) 
 {
-printf("%d is zero\n",n);
+printf("%d is zero\n", n);
 }
 else
 {
