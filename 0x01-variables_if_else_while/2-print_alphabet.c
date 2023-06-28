df337acc9eb754a825d514aadcd2cd14e3a8b4dd
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-06-28 03:40:17 -0700

    Your commit message

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints the lowercase
+ * alphabet followed by a newline character
+ *Only the `putchar` function is allowed to be used
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter;
+for (letter= 'a'; latter <= 'z'; letter++)
+putchar(letter);
+putchar('\n')
+return (0);
+}
