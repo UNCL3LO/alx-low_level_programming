8cf8f2fbee5053bbf158a0d3a1da3b88b7ccf5e6
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-06-28 05:54:56 -0700

    Your commit message

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*more headers goes there
-*main -Entry point
-*@n variable to be set to random value
-*Progran to set variable n to random value
-*Returns message
-*/
+/**
+ * main - Entry point
+ *Description: Sets the variable n to a
+ * random value and prints whether
+ *the number stored in n is positive, zero, or negative.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
