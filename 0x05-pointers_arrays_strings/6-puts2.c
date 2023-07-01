0664bdc1a15e80b5ea2d0ae2dd42d5934293ca15
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-07-01 13:23:33 -0700

    Your commit message

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * puts2 - Prints every other character of a
  * string, starting with the first.
  * @str: The string to be printed.
- *
  * Return: void.
  */
 void puts2(char *str)
@@ -14,8 +13,8 @@ while (str[i] != '\0')
 {
 /* Print the character at even indices */
 if (i % 2 == 0)
-_putchar(str[i]);
+putchar(str[i]);
 i++;
 }
-_putchar('\n');
+putchar('\n');
 }
