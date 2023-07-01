5fcd81fcf20ef81e0705e8b415e940b3ed64b410
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-07-01 12:48:00 -0700

    Your commit message

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,13 @@
 void print_rev(char *s)
 {
 int length = 0;
+int i;
 /* Calculate the length of the string*/
 while (s[length] != '\0')
 length++;
 /* Print the characters in reverse order*/
 int i;
 for (i = length - 1; i >= 0; i--)
-_putchar(s[i]);
-_putchar('\n');
+putchar(s[i]);
+putchar('\n');
 }
