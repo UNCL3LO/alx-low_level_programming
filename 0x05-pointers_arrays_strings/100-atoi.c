d9ae26fc92b93dc45bad187a190895c141a5fc49
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-07-01 14:02:56 -0700

    Your commit message

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+int sign = 1;
+int result = 0;
+int i = 0;
+/* Handling the sign */
+if (s[0] == '-')
+{
+sign = -1;
+i++;
+}
+else if (s[0] == '+')
+{
+i++;
+}
+/* Converting the string to integer */
+while (s[i] != '\0')
+{
+if (s[i] >= '0' && s[i] <= '9')
+{
+result = (result * 10) + (s[i] - '0');
+}
+else
+{
+break;
+}
+i++;
+}
+return (result * sign);
+}
