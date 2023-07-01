f270a535a55061f7df6223480db2839bb8bb09ef
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-07-01 13:20:57 -0700

    Your commit message

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a
+ * string, starting with the first.
+ * @str: The string to be printed.
+ *
+ * Return: void.
+ */
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+/* Print the character at even indices */
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
