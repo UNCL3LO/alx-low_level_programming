6d88f36cf0ac3497b947d3e0f0c28dc78687e444
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-07-03 11:49:39 -0700

    Function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: A pointer to the resulting string @dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+char *ptr = dest; /* Store the pointer to the start of the destination string */
+/* Move the pointer to the end of the destination string */
+while (*ptr != '\0')
+ptr++;
+/* Copy characters from src to dest until the end of src */
+while (*src != '\0')
+{
+*ptr = *src;
+ptr++;
+src++;
+}
+/* Add the terminating null byte at the end */
+*ptr = '\0';
+/* Return a pointer to the resulting string dest */
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+Pointers for final probation day
