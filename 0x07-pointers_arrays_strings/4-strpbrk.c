140b83b28dd19c25542ff7f52887f398b386e35b
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-07-04 06:32:06 -0700

    Function that checks a string for any set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,8 @@
  * @s: The string to search in
  * @accept: The set of bytes to search for
  *
- * Return: A pointer to the byte in s that matches one of the bytes in accept,
+ * Return: A pointer to the byte in s that
+ * matches one of the bytes in accept,
  *         or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
