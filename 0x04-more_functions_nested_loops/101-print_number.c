49010a94e7f44dfdebc00fa0ddd7b67a046cc69b
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-07-04 12:11:53 -0700

    Function that prints a triangle

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+    int divisor = 1;
+    int digit, i;
+if (n < 0) {
+_putchar('-');
+n = -n;
+}
+while (n / divisor >= 10)
+divisor *= 10;
+while (divisor > 0) {
+digit = (n / divisor) % 10;
+_putchar(digit + '0');
+divisor /= 10;
+}
+}
