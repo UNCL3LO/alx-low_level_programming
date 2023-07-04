35ac3b822318ae2df8ed08f96a2f1cf2bdf7a10d
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-07-04 12:08:35 -0700

    Function that prints a triangle

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,5 +14,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+long long largestPrimeFactor(long long n);
 
 #endif /* MAIN_H */
