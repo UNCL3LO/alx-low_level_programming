235233edb39039436a37ebe97adbc55ff6b89f38
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-07-04 10:13:49 -0700

    function that a number is between 0 and 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+/**
+ * _isdigit - checks if a number is between 0 and 9
+ * @c: the integer to be checked
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (c >= 0 && c <9 )
+return (1);
+else
+return (0);
+}
