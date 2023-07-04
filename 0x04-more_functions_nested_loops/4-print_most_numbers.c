ae314a1b21375c65849e368af4c8615c923f5fd1
UNCL3LO <collinsmunenemutwiri@gmail.com>
2023-07-04 10:44:20 -0700

    function that prints the numbers, from 0 to 9, excluding 2 and 4 followed by a new line.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_most_numbers - prints numbers from 0 to 9 excluding 2 and 4
+ * print_most_numbers - prints numbers
+ * from 0 to 9 excluding 2 and 4
  */
 void print_most_numbers(void)
 {
